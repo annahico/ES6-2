f8bb74f3e55738ee5d19f0cc22d8684da07044a9
annahico <annahico@gmail.com>
2024-04-17 20:04:30 +0200

    suma y resta

---
+++ js/calculadora.js
@@ -0,0 +1,22 @@
+class Calculadora {
+    // static PI = 3.14;
+    static IVA = 0.21;
+
+    static suma(a, b) {
+        return a + b;
+    }
+    static resta(a, b) {
+        return (a - b);
+    }
+}
+
+
+let resultado = Calculadora.suma(3, 6);
+console.log(resultado);
+
+
+
+// let calculadora = new Calculadora();
+// Calculadora.PI;
+// Math.PI;
+
